83c23eb3428b57da26fc2b109193bb60eb63eba1
nerbyk <danil.maximov2000@gmail.com>
2021-09-28 17:16:03 +0200

    overwrite responder class routing logic

--- app/bot/dispatcher/super_base.rb
+++ app/bot/dispatcher/super_base.rb
@@ -5,7 +5,7 @@ module Dispatcher
     extend Dry::Initializer
 
     param :user, Types::Instance(::User)
-    option :message, optional: true
+    option :message
     option :client
     option :force_step, optional: true
 
@@ -23,8 +23,19 @@ module Dispatcher
 
     protected
 
-    def responder_class(role = user.role, step = user.step)
-      ::Responders::ResponderClassService.call(role, step)
+    def responder_class(role = user.role, step = nil)
+      step ||= message[:content][:step] || user.step
+
+      namespace = Object.const_get "Dispatcher::#{role.capitalize}"
+
+      responders = namespace.constants.filter { |responder| responder.to_s.include? 'Responder' }
+      
+      klass = responders.find do |const|
+        responder = Object.const_get"#{namespace}::#{const}"
+        responder.instance_methods.include? step.to_sym
+      end
+
+      Object.const_get "#{namespace}::#{klass || 'BaseResponder'}"
     end
   end
 end
